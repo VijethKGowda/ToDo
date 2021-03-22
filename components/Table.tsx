98e1cd61b4755b73271548fc4c7eef61181b783e
vijethkgowda <vijethkgowda@gmail.com>
2021-03-22 10:15:56 +0530

    button width

--- components/Table.tsx
+++ components/Table.tsx
@@ -308,7 +308,7 @@ const Table: React.FunctionComponent<TableProps> = ({
                                     onClick={() => {
                                       changeStatus(tab, index, "done");
                                     }}
-                                    className="mr-3 px-3 py-2 border w-20 text-white border-gray-300 rounded-md shadow-sm text-sm bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                                    className="mr-3 px-3 py-2 border w-24 text-white border-gray-300 rounded-md shadow-sm text-sm bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
                                   >
                                     Done
                                   </button>
@@ -318,7 +318,7 @@ const Table: React.FunctionComponent<TableProps> = ({
                                     onClick={() => {
                                       changeStatus(tab, index, "in_progress");
                                     }}
-                                    className="mr-3 px-3 py-2 border w-20 text-white border-gray-300 rounded-md shadow-sm text-sm bg-gray-500 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                                    className="mr-3 px-3 py-2 border w-24 text-white border-gray-300 rounded-md shadow-sm text-sm bg-gray-500 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                                   >
                                     Re open
                                   </button>
@@ -399,7 +399,7 @@ const Table: React.FunctionComponent<TableProps> = ({
                               onClick={() => {
                                 changeStatus(tab, index, "done");
                               }}
-                              className="mr-3 px-3 py-2 border w-20 text-white border-gray-300 rounded-md shadow-sm text-sm bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                              className="mr-3 px-3 py-2 border w-24 text-white border-gray-300 rounded-md shadow-sm text-sm bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
                             >
                               Done
                             </button>
@@ -409,7 +409,7 @@ const Table: React.FunctionComponent<TableProps> = ({
                               onClick={() => {
                                 changeStatus(tab, index, "in_progress");
                               }}
-                              className="mr-3 px-3 py-2 border w-20 text-white border-gray-300 rounded-md shadow-sm text-sm bg-gray-500 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                              className="mr-3 px-3 py-2 border w-24 text-white border-gray-300 rounded-md shadow-sm text-sm bg-gray-500 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                             >
                               Re open
                             </button>
